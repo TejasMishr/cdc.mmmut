f44feb5dd927e69d21bd383837b159a4b514ab1d
TejasMishr <tejasmishra72@gmail.com>
2024-02-05 13:52:08 +0530

    basic structure done

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home/Home";
+import Event from "./pages/Events/Event";
+import Projects from "./pages/Projects/Projects";
+import Team from "./pages/Team/Team";
+import Contact from "./pages/Contact/Contact";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+
+// App component
+
+export default function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/codersclub.mmmut" exact component={Home} />
+          <Route path="/codersclub.mmmut/event" exact component={Event} />
+          <Route path="/codersclub.mmmut/projects" exact component={Projects} />
+          <Route path="/codersclub.mmmut/team" exact component={Team} />
+          <Route path="/codersclub.mmmut/contact" exact component={Contact} />
+          <Redirect to="/codersclub.mmmut/" />
+        </Switch>
+      </Router>
+    </>
+  );
+}
