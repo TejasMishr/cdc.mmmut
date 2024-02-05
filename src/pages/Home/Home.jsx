d200a4542c1e76554410d35acecd358f471d7e62
TejasMishr <tejasmishra72@gmail.com>
2024-02-05 15:10:06 +0530

    ok

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,95 @@
+import React from "react";
+import HeroSection from "../../Components/HeroSection";
+import TitleCenter from "./Components/TitleCenter";
+import Card from "./components/Card";
+import Card_Details from "./data/Card_Details";
+import { homeObjOne, homeObjTwo, homeObjThree, homeObjFour } from "./data/Data";
+import {
+  TitleCenter1,
+  TitleCenter2,
+  TitleCenter3,
+} from "./data/TitleCenterData";
+import Form from "../Home/components/Form";
+import ideaImage from "../../assets/images/ideaImage.svg";
+import Button from "./components/Button";
+import FaQ from "./Components/FaQ";
+import Footer from "../../Components/Footer";
+
+import "../Home/Home.css";
+
+export default function Home() {
+  return (
+    <>
+      {" "}
+      <div className="main">
+        <div className="container _shapeAnimation">
+          <HeroSection {...homeObjOne} />
+          <HeroSection {...homeObjTwo} />
+          <HeroSection {...homeObjThree} />
+          <HeroSection {...homeObjFour} />
+        </div>
+        {/* Technologies section */}{" "}
+        <div className="tech_bg_color">
+          {" "}
+          <div className="container">
+            <div className="text-center">
+              <TitleCenter {...TitleCenter1} />
+            </div>
+            <div className="row d-flex justify-content-center">
+              {Card_Details.map((item, index) => (
+                <div key={index} className="col-lg-6 col-xl-4 col-md-6 mx-auto">
+                  <Card img={item.img} tech_Name={item.tech_Name} />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+        {/* Technologies section ended */} {/* recent_project section */}{" "}
+        <div className="text-center recent_project">
+          {" "}
+          <TitleCenter {...TitleCenter2} />
+          <p
+            style={{
+              color: "grey",
+            }}
+          >
+            No Projects Available Right Now
+          </p>
+        </div>
+        {/* recent_project section ended */}{" "}
+        <div className="form_bg_color idea_container">
+          {" "}
+          <div className="container ">
+            <div className="row main_content">
+              <div className="col-lg-7 col-md-12 mx-auto">
+                <div>
+                  <img className="idea_img" src={ideaImage} alt="Idea-Image" />
+                </div>
+              </div>
+              <div className="col-lg-5 col-md-12 form_container">
+                <div className="form_content">
+                  <Form placeholder="Name*" />
+                  <Form placeholder="Email*" />
+                  <Form placeholder="Branch*" />
+                  <Form placeholder="Year*" />
+                  <Form placeholder="Idea*" />
+                  <Button text="SUBMIT IDEA" column="12" buttonSize="50px" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* FAQ section ----------->>>> */}{" "}
+        <div className="tech_bg_color">
+          {" "}
+          <div className="text-center">
+            <TitleCenter {...TitleCenter3} />
+          </div>
+          <FaQ />
+        </div>
+        {/* FAQ section ----------->>>> end */}{" "}
+        {/* Footer ----------------->>>>> */} <Footer />{" "}
+      </div>
+    </>
+  );
+}
---
+++ src/pages/Home/data/Card_Details.jsx
@@ -0,0 +1,50 @@
+import img1 from "../../../assets/Logos/flutter.svg";
+import img2 from "../../../assets/Logos/reactjs.svg";
+import img3 from "../../../assets/Logos/nextjs.svg";
+import img4 from "../../../assets/Logos/javascript.svg";
+import img5 from "../../../assets/Logos/python.svg";
+import img6 from "../../../assets/Logos/java.svg";
+import img7 from "../../../assets/Logos/robot.svg";
+import img8 from "../../../assets/Logos/firebase.svg";
+import img9 from "../../../assets/Logos/amazon-web-services.svg";
+
+const Card_Details = [
+  {
+    img: img1,
+    tech_Name: "Flutter App Development",
+  },
+  {
+    img: img2,
+    tech_Name: "React Js Development",
+  },
+  {
+    img: img3,
+    tech_Name: "Next Js Development",
+  },
+  {
+    img: img4,
+    tech_Name: "JavaScript",
+  },
+  {
+    img: img5,
+    tech_Name: "Python Programming",
+  },
+  {
+    img: img6,
+    tech_Name: "Java Programming",
+  },
+  {
+    img: img7,
+    tech_Name: "Machine Learning",
+  },
+  {
+    img: img8,
+    tech_Name: "Firebase",
+  },
+  {
+    img: img9,
+    tech_Name: "Amazon Web Services",
+  },
+];
+
+export default Card_Details;
