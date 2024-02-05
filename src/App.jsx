06e1731bd8396207c254d0d065e21967e5b202bb
TejasMishr <tejasmishra72@gmail.com>
2024-02-05 14:02:11 +0530

    basic structure done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "./components/Navbar";
+import Navbar from "./Components/Navbar";
 import Home from "./pages/Home/Home";
 import Event from "./pages/Events/Event";
 import Projects from "./pages/Projects/Projects";
