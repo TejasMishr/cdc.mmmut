ce706e511a5b0436cd90ec32d738d12a817af461
TejasMishr <tejasmishra72@gmail.com>
2024-02-06 13:14:39 +0530

    firebase links added

--- src/pages/Home/data/Card_Details.jsx
+++ src/pages/Home/data/Card_Details.jsx
@@ -6,14 +6,17 @@ const link1 =
 const link2 =
   "https://firebasestorage.googleapis.com/v0/b/evna-1c728.appspot.com/o/img%2Freact.png?alt=media&token=90115ef1-0275-4ceb-8a81-3173de2d2a00";
 
-const link5 =
-  "https://firebasestorage.googleapis.com/v0/b/evna-1c728.appspot.com/o/img%2Fpy.png?alt=media&token=4c7c6682-14b4-41b3-90fb-e435fc9b4b5a";
-
 const link3 =
   "https://firebasestorage.googleapis.com/v0/b/evna-1c728.appspot.com/o/img%2Fnode.png?alt=media&token=915a2496-2a9a-45d6-96ba-1c8bf2eaa504";
 
 const link4 =
   "https://firebasestorage.googleapis.com/v0/b/evna-1c728.appspot.com/o/img%2Fjavascript.png?alt=media&token=bbed2785-213e-4fd9-bb50-8992cde82f26";
+
+const link5 =
+  "https://firebasestorage.googleapis.com/v0/b/evna-1c728.appspot.com/o/img%2Fpy.png?alt=media&token=4c7c6682-14b4-41b3-90fb-e435fc9b4b5a";
+
+const link6 =
+  "https://firebasestorage.googleapis.com/v0/b/evna-1c728.appspot.com/o/img%2Fjava.png?alt=media&token=1351eafc-5818-4190-b287-eecfee38e4bd";
 
 const link7 =
   "https://firebasestorage.googleapis.com/v0/b/evna-1c728.appspot.com/o/img%2Fmachine.png?alt=media&token=6c5ce955-262f-4b9e-b4c5-6d4fceb7e334";
@@ -21,8 +24,6 @@ const link7 =
 const link8 =
   "https://firebasestorage.googleapis.com/v0/b/evna-1c728.appspot.com/o/img%2Ffirebase.png?alt=media&token=5faaac2f-0a65-40c8-8e9c-30fbe5768e46";
 
-const link6 =
-  "https://firebasestorage.googleapis.com/v0/b/evna-1c728.appspot.com/o/img%2Fjava.png?alt=media&token=1351eafc-5818-4190-b287-eecfee38e4bd";
 const link9 =
   "https://firebasestorage.googleapis.com/v0/b/evna-1c728.appspot.com/o/img%2Faws.png?alt=media&token=a3a47544-414e-46a9-adf5-dfa1d3a91730";
 const Card_Details = [
